d7278dee65e64824d029cd6a85a226bb38491c9f
Jorgenssen <n.tatyannikov@gmail.com>
2024-04-06 01:17:00 +0100

    Test commit for webhook 1

--- src/main/java/org/me/solution/Solution.java
+++ src/main/java/org/me/solution/Solution.java
@@ -12,13 +12,16 @@ public class Solution {
         if (ransomNote.length() > magazine.length()) {
             return false;
         }
+
         Map<Character, Integer> magazineLetters = new HashMap<>();
+
         for (int i = 0; i < magazine.length(); i++) {
             char m = magazine.charAt(i);
 
             int currentCount = magazineLetters.getOrDefault(m, 0);
             magazineLetters.put(m, ++currentCount);
         }
+
         for (int j = 0; j < ransomNote.length(); j++) {
             char r = ransomNote.charAt(j);
             int currentCount = magazineLetters.getOrDefault(r, 0);
